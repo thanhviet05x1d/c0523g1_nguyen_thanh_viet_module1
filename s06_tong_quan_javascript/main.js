84324626ede2c81aa9635f281ad28cdaf33b499c
viet <thanhvietbkdn@gmail.com>
2023-06-07 10:30:41 +0700

    dua bai len

---
+++ s06_tong_quan_javascript/main.js
@@ -0,0 +1,12 @@
+function showMess1(){
+    alert("Xin chao ban 1");
+}
+function showMess2(){
+    document.getElementById("divMess").innerHTML="Xin chao ban";
+}
+function showMess3(){
+    document.write("Xin chao ban 3");
+}
+function showMess4(){
+    console.log("Xin chao ban 4");
+}
